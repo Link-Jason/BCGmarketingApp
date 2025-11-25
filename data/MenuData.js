eaae6680c8ea7ad4ebbe4b80cd9e8dd801a74adc
Jason Link <your.github.jason.link@yahoo.com>
2025-11-25 08:43:22 +0200

    FIX: Resolved syntax error in MenuData.js and updated data structure

---
+++ data/MenuData.js
@@ -0,0 +1,19 @@
+// src/data/MenuData.js
+
+/**
+ * Data for BCG Matrix analysis. Revenue is used for bubble size.
+ * Volume is used to calculate market growth.
+ */
+export const RAW_MENU_ITEMS = [
+  {
+    name: "Espresso Shot (Dog)",
+    revenue: 500.00,
+    volume: 500
+  },
+  {
+    name: "House Blend Coffee (Cash Cow)",
+    revenue: 15000.00,
+    volume: 1500
+  },
+  // ... include the rest of your items here
+];
